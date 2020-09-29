5f7ba90b445b031718c0723ec32dc96825567ed1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-29 23:06:19 +1000

    new article on events

--- src/articles.ts
+++ src/articles.ts
@@ -6,5 +6,11 @@ export const articles: Article[] = [
     slug: "patterns-for-testing-props",
     published: "2020-09-22",
     description: "Learn about testing props in Vue apps, and how testing props can verify whether you are correctly separating your business logic and your UI.",
+  },
+  {
+    title: "Patterns for Testing Events",
+    slug: "patterns-for-testing-events",
+    published: "2020-09-29",
+    description: "Some patterns, tips and tricks for consistency and reliability when emitting events in Vue components.",
   }
 ]
