e35acd1879162d588cff0de747484da81ce2c021
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-30 23:12:40 +1000

    meta

--- app/controllers.ts
+++ app/controllers.ts
@@ -6,9 +6,10 @@ const router = Router()
 
 router.use('/articles/:slug', (req: Request, res: Response) => {
   const content = fs.readFileSync(`./app/public/articles/${req.params.slug}.html`)
-  res.render('articles/show', { content })
+  const article = articles.find(x => x.slug === req.params.slug)
+  console.log(article)
+  res.render('articles/show', { content, article })
 })
-
 
 router.use('/articles', (req: Request, res: Response) => {
   res.render('articles/index', { articles })
