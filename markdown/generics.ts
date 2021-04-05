2589192a05d4d350290c0ba94773355b89d0ab00
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-05 18:15:23 +1000

    update

--- markdown/generics.ts
+++ markdown/generics.ts
@@ -26,3 +26,19 @@ function shift<T>(array: T[]): T | undefined {
   array = rest
   return first
 }
+
+function useState<S>(initialState: S): [S, (newValue: S) => void] {
+  function updateState(newValue: S) {
+    // ... react magic ...
+  }
+
+  return [
+    initialState,
+    updateState
+  ]
+}
+
+
+const [count, setCount] = useState(5)
+
+const [person, setPerson] = useState<Person>({ id: 1, name: 'Alice' })
